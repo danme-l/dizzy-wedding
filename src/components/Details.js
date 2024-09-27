13a7f85f17b8d08c0f221e00275860fd7f9b5c9f
dan <meleras.daniel@icloud.com>
2024-09-27 07:32:16 +0200

    added user validation, some styling

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,8 +7,8 @@ const Details = () => {
       <Typography variant="h4" gutterBottom>
         Wedding Details
       </Typography>
-      <Typography variant="body1">Date: November 1, 2024</Typography>
-      <Typography variant="body1">Location: Ottawa</Typography>
+      <Typography variant="body1">Date: January 10, 2026</Typography>
+      <Typography variant="body1">Location: Montreal</Typography>
       <Typography variant="body1">Time: Noon</Typography>
     </Container>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,48 @@
-import React from 'react';
-import { Box, Grid, Typography, Paper} from '@mui/material';
+import React, {useState} from 'react';
+import { Box, Grid, Typography, Paper, TextField, Button, Container} from '@mui/material';
+
+// temporary
+const validCodes = {
+  '1': 'Laura Clark and Jeff Meleras, Parents of the Groom',
+  '2': 'Claudine Michaud and Adnan Hadziomerovic, Parents of the Bride',
+  '3': 'Tommy Wallis',
+  '4': 'Lara Kercoglu',
+  '5': 'Caroline Meleras',
+  '6': 'Jacob Meleras, Best Man',
+  '7': 'Ariane Hadziomerovic, Maid of Honour',
+  '8': 'Alexa Hadziomerovic, Maid of Honour',
+};
+
+
 const Home = () => {
+  // **SECTION** guest validation
+  // to be hooked up to a database eventually 
+  const [code, setCode] = useState(0)
+  const [name, setName] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  // valid code for demonstration purposes
+  const validCode = '2';
+
+
+  // gets the code out of the textbox
+  const handleInputChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  // sets the user code as the State
+  // feature desire: the code will query a db where there will be a guest-code mapping 
+  // from there, 
+  const handleSubmit = () => {
+    if (validCodes[code]) {
+      setName(validCodes[code]);
+    } else {
+      alert('Invalid code. Please try again.');
+    }
+  };
+  
+
+  // **SECTION** Date Stuff
   const weddingDate = new Date('10 January 2026 12:00 EST');
   const dateOptions = {
     weekday: 'long',
@@ -8,7 +50,6 @@ const Home = () => {
     month: 'long',
     day: 'numeric',
   };
-
   // date calculator
   const dateCalculate = (target) => {
     const now = new Date();
@@ -24,35 +65,59 @@ const Home = () => {
 
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <Box sx={{
-          m: "50px",
-          p: "20px"
-        }}>
-          <Typography variant="h1"> 
-            The Marriage of Isabel Haziomeric & Daniel Meleras
+    <Container>
+      {!name ? (
+        <Box>
+          <Typography variant="h4" gutterBottom>
+            Enter Your Code
           </Typography>
-          <Typography variant='h2' mx={3}>
-            {weddingDate.toLocaleDateString('en-CA', dateOptions)}
-          </Typography>
-          <Typography variant='h2' mx={3}>
-            {dateCalculate(weddingDate)}
-          </Typography>
+          <Box  sx={{display:'flex', p:2, gap: 2}}>
+            <TextField
+              label="Invite Code"
+              variant="outlined"
+              value={code}
+              onChange={handleInputChange}
+              />
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Box>
         </Box>
+      ) :  
+      (<Grid container spacing={1}>
+        <Grid item xs={12} md={6}>
+          <Box sx={{
+            p: 1,
+            gap: 1
+          }}>
+            <Typography variant='h2'>
+              Welcome, {name}
+            </Typography>
+            <Typography variant="h1"> 
+              The Marriage of Isabel Haziomeric & Daniel Meleras
+            </Typography>
+            <Typography variant='h2' mx={3}>
+              {weddingDate.toLocaleDateString('en-CA', dateOptions)}
+            </Typography>
+            <Typography variant='h2' mx={3}>
+              {dateCalculate(weddingDate)}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            component="img"
+            sx={{
+              width: '84%',
+              m: 0
+            }}
+            alt="Dan and Izzy"
+            src="static/images/danIzzy1.jpg"
+            />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Box
-          component="img"
-          sx={{
-            width: '84%',
-            m: 0
-          }}
-          alt="Dan and Izzy"
-          src="static/images/danIzzy1.jpg"
-          />
-      </Grid>
-    </Grid>
+      )}
+    </Container>
   );
 };
 
