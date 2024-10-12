aeda937b3c51c3029d66ca87c480476bdb88b980
Daniel Meleras <meleras.daniel@icloud.com>
2024-10-12 12:56:42 +0200

    Fix auth (#6)
    * authorization fixes
    
    * minor cleaning
    
    * add comments

--- src/App2.js
+++ src/App2.js
@@ -1,4 +1,5 @@
-import {Box, Typography, AppBar, Toolbar, Button, Paper, Menu, MenuItem, Drawer, List, ListItemText } from '@mui/material';
+import { useState, useEffect } from 'react';
+import {Box, Typography, TextField, AppBar, Toolbar, Button, Paper, Menu, MenuItem, Drawer, List, ListItemText } from '@mui/material';
 import {IconButton, ListItem, ListItemButton, Divider} from '@mui/material';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -23,6 +24,17 @@ const mainMenuArr = ["Home", "Details", "About Us", "RSVP"];
 const detailsMenuArr = ["Details", "Schedule", "FAQ"];
 const aboutusMenuArr = ["Our Story", "Gallery", "About Us"];
 
+// guests (temporary)
+const validCodes = {
+  '1': 'Laura Clark and Jeff Meleras, Parents of the Groom',
+  '2': 'Claudine Michaud and Adnan Hadziomerovic, Parents of the Bride',
+  '3': 'Tommy Wallis',
+  '4': 'Lara Kercoglu',
+  '5': 'Caroline Meleras',
+  '6': 'Jacob Meleras, Best Man',
+  '7': 'Ariane Hadziomerovic, Maid of Honour',
+  '8': 'Alexa Hadziomerovic, Maid of Honour',
+};
 
 const StyledMenu = styled((props) => (
   <Menu
@@ -100,13 +112,16 @@ function SmallMenuIcon(openMenu, setOpenMenu){
   );
 };
 
+// **SECTION** small menu appears on phones 
 function SmallMenu(theme, openMenu, setOpenMenu) {
+  // open, close handlers
   const handleDrawerClose = () => {
     setOpenMenu(false);
   };
   const handleDrawerOpen = () => {
     setOpenMenu(true);
   };
+
 
   return (
     <Drawer
@@ -122,13 +137,18 @@ function SmallMenu(theme, openMenu, setOpenMenu) {
       anchor="right"
       open={openMenu}
     >
+      {/* drawer header, containing the close button */}
       <DrawerHeader>
         <IconButton onClick={handleDrawerClose}>
           <ChevronRight />
         </IconButton>
       </DrawerHeader>
       <Divider />
+
+      {/* navigation list */}
       <List disablePadding>
+
+        {/* ITEM 1: Home */}
           <ListItem key='Home' disablePadding>
             <ListItemButton
               component={Link}
@@ -145,45 +165,51 @@ function SmallMenu(theme, openMenu, setOpenMenu) {
               
             </ListItemButton>
           </ListItem>
-        <Divider/>
-        <ListItem key='Details' disablePadding sx={{width: "100%"}}>
-          <Accordion style={{m: 0, boxShadow: "none", width: "12em"}} square={true} disableGutters={true}
-            sx={{'&:before': {
-              display: 'none',
-            }
-            }}>
-            <AccordionSummary
-              expandIcon={<ArrowDropDownIcon />}
-              aria-controls="panel1-content"
-              id="panel1-header">
-              <Typography
-                disableElevation
-                color="inherit"
-                sx={{
-                  fontStyle: 'italic',
-                  textTransform: 'none',
-                }}>
-                  Details
-                </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              {detailsMenuArr.map((text, index) => (
-              <Button key={text}
-                component={Link}
-                fullWidth={true}
-                sx={{
-                  justifyContent: 'flex-start',
-                  '&:hover': {
-                  backgroundColor: theme.palette.background.default, // Highlight on hover
-                },}}
-                to={'/' + text.replace(/\s/g, '').toLowerCase()}>
-                {text}
-              </Button>
-              ))}
-            </AccordionDetails>
-          </Accordion>
-        </ListItem>
-      <Divider/>
+          <Divider/>
+
+          {/* ITEM 2: details */}
+          <ListItem key='Details' disablePadding sx={{width: "100%"}}>
+            <Accordion style={{m: 0, boxShadow: "none", width: "12em"}} square={true} disableGutters={true}
+              sx={{'&:before': {
+                display: 'none',
+              }
+              }}>
+              <AccordionSummary
+                expandIcon={<ArrowDropDownIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header">
+                <Typography
+                  disableElevation
+                  color="inherit"
+                  sx={{
+                    fontStyle: 'italic',
+                    textTransform: 'none',
+                  }}>
+                    Details
+                  </Typography>
+              </AccordionSummary>
+
+              {/* submenu, iterating over the details and creating links */}
+              <AccordionDetails>
+                {detailsMenuArr.map((text, index) => (
+                <Button key={text}
+                  component={Link}
+                  fullWidth={true}
+                  sx={{
+                    justifyContent: 'flex-start',
+                    '&:hover': {
+                    backgroundColor: theme.palette.background.default, // Highlight on hover
+                  },}}
+                  to={'/' + text.replace(/\s/g, '').toLowerCase()}>
+                  {text}
+                </Button>
+                ))}
+              </AccordionDetails>
+            </Accordion>
+          </ListItem>
+          <Divider/>
+
+          {/* ITEM 3: about us */}
           <ListItem key='AboutUs' disablePadding>
             <Accordion style={{m: 0, boxShadow: "none", width: "12em"}} square={true} disableGutters={true}
               sx={{'&:before': {
@@ -203,6 +229,8 @@ function SmallMenu(theme, openMenu, setOpenMenu) {
                   About Us
                 </Typography>
               </AccordionSummary>
+
+              {/* submenu, iterating over the details and creating links */}
               <AccordionDetails>
                 {aboutusMenuArr.map((text, index) => (
                   <Button key={text}
@@ -220,8 +248,9 @@ function SmallMenu(theme, openMenu, setOpenMenu) {
               </AccordionDetails>
             </Accordion>
           </ListItem>
+          <Divider/>
 
-      <Divider/>
+          {/* ITEM 4: rsvp */}
           <ListItem key='RSVP' disablePadding>
             <ListItemButton 
               component={Link}
@@ -237,23 +266,31 @@ function SmallMenu(theme, openMenu, setOpenMenu) {
               <ListItemText primary={'RSVP'} />
             </ListItemButton>
           </ListItem>
+
       </List>
     </Drawer>
   );
 };
 
+// **SECTION** large menu appears on large screens such as laptops, iPads
 function LargeMenu(theme) {
   //const theme = useTheme();
+
+  // state, handlers for the details dropdown
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  // state, handlers for the about us dropdown
   const [anchorEl2, setAnchorEl2] = React.useState(null);
   const open2 = Boolean(anchorEl2);
+
   const handleClick2 = (event2) => {
     setAnchorEl2(event2.currentTarget);
   };
@@ -262,7 +299,10 @@ function LargeMenu(theme) {
   };
 
   return (
+    // Container for the nav buttons 
     <Box>
+
+      {/* SECTION Home */}
       <Button
         component={Link}
         to="/"
@@ -278,6 +318,8 @@ function LargeMenu(theme) {
         Home
       </Button>
       {/*Maybe do a hover brings on the menu as well?*/}
+
+      {/* SECTION details button, including dropdown */}
       <Button
         aria-controls={open ? 'details-menu' : undefined}
         aria-haspopup="true"
@@ -297,6 +339,8 @@ function LargeMenu(theme) {
       >
       Details
       </Button>
+
+      {/* SECTION about us button, including dropdown */}
       <Button
         aria-controls={open2 ? 'about-menu' : undefined}
         aria-haspopup="true"
@@ -316,11 +360,14 @@ function LargeMenu(theme) {
         >
         About Us
       </Button>
+
+      {/* SECTION details menu */}
       <StyledMenu id="details-menu"
         MenuListProps={{ 'aria-labelledby': 'details-button',}}
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}>
+        {/* iterate over the details menu to create menu items */}
         {detailsMenuArr.map((text, index) => (
           <MenuItem key={text} onClick={handleClose} disableRipple
           component={Link}
@@ -329,11 +376,14 @@ function LargeMenu(theme) {
         </MenuItem>
         ))}
       </StyledMenu>
+
+      {/* SECTION about us menu */}
       <StyledMenu id="about-menu"
         MenuListProps={{ 'aria-labelledby': 'about-button',}}
         anchorEl={anchorEl2}
         open={open2}
         onClose={handleClose2}>
+        {/* iterate over the about us menu to create menu items */}
         {aboutusMenuArr.map((text, index) => (
           <MenuItem key={text} onClick={handleClose2} disableRipple
             component={Link}
@@ -342,6 +392,8 @@ function LargeMenu(theme) {
           </MenuItem>
         ))}
       </StyledMenu>
+
+      {/* SECTION rsvp button */}
       <Button
         color="inherit"
         component={Link}
@@ -364,6 +416,35 @@ const App = () => {
   const theme = useTheme();
   const [openMenu, setOpenMenu] = React.useState(false);
 
+  // **SECTION** guest validation
+  // to be hooked up to a database eventually 
+  const [code, setCode] = useState(0)
+  const [name, setName] = useState('');
+
+  // assume name in non-production environments
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') {
+      // random name
+      setName('Let\'ss go');
+    }
+  }, []);
+
+  // gets the code out of the textbox
+  const handleInputChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  // sets the user code as the State
+  // feature desire: the code will query a db where there will be a guest-code mapping 
+  // from there, the user will be able to see information specific to their attendance
+  // eg: wedding party will see rehearsal dinner, morning of information
+  const handleSubmit = () => {
+    if (validCodes[code]) {
+      setName(validCodes[code]);
+    } else {
+      alert('Invalid code. Please try again.');
+    }
+  };
 
   return (
     <Router>
@@ -385,6 +466,7 @@ const App = () => {
             >
               D & I
             </Typography>
+
             {/* Links on the right */}
             {/*Use media query to determine if this should show up */}
             {useMediaQuery(theme.breakpoints.down('md')) ? 
@@ -392,14 +474,38 @@ const App = () => {
             
           </Toolbar>
         </AppBar>
+
         {/* add drawer for small menu here*/}
         {useMediaQuery(theme.breakpoints.down('md')) && 
           SmallMenu(theme, openMenu, setOpenMenu)}
-        <div><image href="https://img.freepik.com/premium-photo/dusty-blue-floral-boho-navy-blue-cream-gold-bouquet_887552-29123.jpg?w=1800"/></div>
+        <div>
+          <image href="https://img.freepik.com/premium-photo/dusty-blue-floral-boho-navy-blue-cream-gold-bouquet_887552-29123.jpg?w=1800"/>
+        </div>
+        
         {/* SECTION Content */}
-        <Paper elevation={3} sx={{m: 3, p: 2}}>
+        {!name ? (
+          <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+            <Box>
+            <Typography variant="h4" gutterBottom>
+              Enter Your Code
+            </Typography>
+            <Box  sx={{display:'flex', p:2, gap: 2}}>
+              <TextField
+                label="Invite Code"
+                variant="outlined"
+                value={code}
+                onChange={handleInputChange}
+                />
+              <Button variant="contained" color="primary" onClick={handleSubmit}>
+                Submit
+              </Button>
+              </Box>
+            </Box>
+          </Box>
+      ) : 
+        (<Paper elevation={3} sx={{m: 3, p: 2}}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home name={name} />} />
             <Route path="/details" element={<Details />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/ourstory" element={<OurStory />} />
@@ -409,6 +515,7 @@ const App = () => {
             <Route path="/faq" element={<FAQ />} />s
           </Routes>
         </Paper>
+        )}
       </Box>
     </Router>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,8 @@
 import React, {useState} from 'react';
 import { Box, Grid, Typography, TextField, Button, Container} from '@mui/material';
 
-// temporary
-const validCodes = {
-  '1': 'Laura Clark and Jeff Meleras, Parents of the Groom',
-  '2': 'Claudine Michaud and Adnan Hadziomerovic, Parents of the Bride',
-  '3': 'Tommy Wallis',
-  '4': 'Lara Kercoglu',
-  '5': 'Caroline Meleras',
-  '6': 'Jacob Meleras, Best Man',
-  '7': 'Ariane Hadziomerovic, Maid of Honour',
-  '8': 'Alexa Hadziomerovic, Maid of Honour',
-};
 
-
-const Home = () => {
-  // **SECTION** guest validation
-  // to be hooked up to a database eventually 
-  const [code, setCode] = useState(0)
-  const [name, setName] = useState('');
-
-  // gets the code out of the textbox
-  const handleInputChange = (event) => {
-    setCode(event.target.value);
-  };
-
-  // sets the user code as the State
-  // feature desire: the code will query a db where there will be a guest-code mapping 
-  // from there, the user will be able to see information specific to their attendance
-  // eg: wedding party will see rehearsal dinner, morning of information
-  const handleSubmit = () => {
-    if (validCodes[code]) {
-      setName(validCodes[code]);
-    } else {
-      alert('Invalid code. Please try again.');
-    }
-  };
-  
+const Home = ({ name }) => {
 
   // **SECTION** Date Stuff
   const weddingDate = new Date('10 January 2026 12:00 EST');
@@ -58,42 +24,14 @@ const Home = () => {
 
   return (
     <Container>
-      {/* 
-      TODO: this needs to be moved to <App>.
-      Temporary: if else handles user access controls.
-      If there is any of the above codes (1-8), it'll let you in. 
-      Eventually these codes will be hashes (?) where the user will enter the code
-      and a db will be queried getting specific information and generating content 
-      specific to that user
-      */}
-      {/* if no current name is in the state, show this code-entry box */}
-      {!name ? (
-        <Box>
-          <Typography variant="h4" gutterBottom>
-            Enter Your Code
-          </Typography>
-          <Box  sx={{display:'flex', p:2, gap: 2}}>
-            <TextField
-              label="Invite Code"
-              variant="outlined"
-              value={code}
-              onChange={handleInputChange}
-              />
-            <Button variant="contained" color="primary" onClick={handleSubmit}>
-              Submit
-            </Button>
-          </Box>
-        </Box>
-      ) :  
-      // if there is a name, show the content
-      (<Grid container spacing={1}>
+      <Grid container spacing={1}>
         <Grid item xs={12} md={6}>
           <Box sx={{
             p: 1,
             gap: 1
           }}>
             <Typography variant='h2'>
-              Welcome, {name}
+              Welcome, {name}!
             </Typography>
             <Typography variant="h1"> 
               The Marriage of Isabel Haziomeric & Daniel Meleras
@@ -118,7 +56,6 @@ const Home = () => {
             />
         </Grid>
       </Grid>
-      )}
     </Container>
   );
 };
