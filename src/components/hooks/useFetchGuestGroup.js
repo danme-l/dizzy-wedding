b33249849021ff6517a86458d3c9ac55095505c2
dan <meleras.daniel@icloud.com>
2025-03-01 13:41:24 +0100

    update hook to use env vars

--- src/components/hooks/useFetchGuestGroup.js
+++ src/components/hooks/useFetchGuestGroup.js
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+
 function useFetchGuestGroup() {
   const [guests, setGuests] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+
   const fetchGuestGroup = async (pw) => {
+    const apiUrl = process.env.REACT_APP_API_URL;
     setLoading(true);
     setError(null);
+
     try {
-      const response = await axios.get(`http://localhost:3000/passwords/${pw}`);
+      const response = await axios.get(`${apiUrl}/passwords/${pw}`);
       if (response.data && response.data.length > 0) {
         setGuests(response.data);
         return true; // fetch was successful and guests w/ that password found
