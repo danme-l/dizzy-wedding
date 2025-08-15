15e2e2e5cbaff159bf98751fccf35bc777d9ec80
Dan <dan6661.accounts@proton.me>
2025-08-15 15:23:00 +0200

    under contruction page

---
+++ src/components/pages/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const UnderConstruction = () => {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        We're working on this page... stay tuned.
+      </Typography>
+    </Container>
+  );
+};
+
+export default UnderConstruction;
